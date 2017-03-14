57ad194671491c3388a7d68e159528ccaeaf1dde
Adikan Wiering <adwiering@gmail.com>
2017-03-14 13:23:41 -0500

    Character Class Unit Tests
    Added Unit tests for the Character Class. They currently have not been
    run due to compiling errors caused by missing XML files.

---
+++ RPG_Fitness_App/app/src/test/java/co/rpg_fitness_app/android/rpg_fitness_app/CharacterTest.java
@@ -0,0 +1,226 @@
+package co.rpg_fitness_app.android.rpg_fitness_app;
+
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by awhit on 3/14/2017.
+ */
+public class CharacterTest {
+    @Test
+    public void getDefaultInactiveGear() throws Exception {
+        Character testChar = new Character();
+        assertNull(testChar.getInactiveGear());
+    }
+
+    @Test
+    public void setInactiveGear() throws Exception {
+        Character testChar = new Character();
+
+        Gear helmOfSorrow =  new Gear();
+        helmOfSorrow.setName("helm of Sorrow");
+        ArrayList<Gear> test = new ArrayList<Gear>();
+        test.add(helmOfSorrow);
+
+        testChar.setInactiveGear(test);
+
+        assertEquals(test, testChar.getInactiveGear());
+    }
+
+    @Test
+    public void getDefaultActiveGear() throws Exception {
+        Character testChar = new Character();
+        assertNull(testChar.getActiveGear());
+    }
+
+    @Test
+    public void setActiveGear() throws Exception {
+        Character testChar = new Character();
+
+        Gear helmOfSorrow =  new Gear();
+        helmOfSorrow.setName("helm of Sorrow");
+        ArrayList<Gear> test = new ArrayList<Gear>();
+        test.add(helmOfSorrow);
+
+        testChar.setActiveGear(test);
+
+        assertEquals(test, testChar.getActiveGear());
+    }
+
+    @Test
+    public void getDefaultMySpecies() throws Exception {
+        Character testChar = new Character();
+        assertNull(testChar.getMySpecies());
+    }
+
+    @Test
+    public void setMySpecies() throws Exception {
+        Character testChar = new Character();
+        Species Orc = new Species();
+        Orc.setDescription("A fearsome greenskin Humanoid");
+        Orc.setName("Orc");
+        testChar.setMySpecies(Orc);
+
+
+        assertEquals(Orc, testChar.getMySpecies());
+
+
+    }
+
+    @Test
+    public void getDefaultName() throws Exception {
+        Character testChar = new Character();
+        assertNull(testChar.getName());
+    }
+
+    @Test
+    public void setName() throws Exception {
+        Character testChar = new Character();
+        testChar.setName("Beowulf");
+        assertSame("Beowulf", testChar.getName());
+    }
+
+    @Test
+    public void getDefaultActiveBoosts() throws Exception {
+        Character testChar = new Character();
+        assertNull(testChar.getActiveBoosts());
+    }
+
+    @Test
+    public void setActiveBoosts() throws Exception {
+        Character testChar = new Character();
+
+        Boost testBoost = new Boost();
+        testBoost.setBoostType("wood");
+        testBoost.setAmount(100);
+
+        ArrayList<Boost> boosts = new ArrayList<Boost>();
+        boosts.add(testBoost);
+
+        testChar.setActiveBoosts(boosts);
+        assertEquals(boosts, testChar.getActiveBoosts());
+
+
+
+    }
+
+    @Test
+    public void getDefaultMoneyChest() throws Exception {
+        Character testChar = new Character();
+        assertNull(testChar.getMoneyChest());
+    }
+
+    @Test
+    public void setMoneyChest() throws Exception {
+        Character testChar = new Character();
+
+        Currency testCurr = new Currency(50, "gold");
+        ArrayList<Currency> currList = new ArrayList<Currency>();
+        currList.add(testCurr);
+
+        testChar.setMoneyChest(currList);
+        assertEquals(currList, testChar);
+
+    }
+
+    @Test
+    public void getInvetory() throws Exception {
+        Character testChar = new Character();
+
+        Gear helmOfSorrow =  new Gear();
+        helmOfSorrow.setName("helm of Sorrow");
+
+        Gear helmOfAnger =  new Gear();
+        helmOfSorrow.setName("helm of Anger");
+
+        Gear swordOfMight =  new Gear();
+        helmOfSorrow.setName("Sword of Might");
+
+
+        ArrayList<Gear> test = new ArrayList<Gear>();
+
+        testChar.addGear(helmOfSorrow);
+        testChar.addGear(helmOfAnger);
+        testChar.addGear(swordOfMight);
+
+
+        test.add(helmOfSorrow);
+        test.add(helmOfAnger);
+        test.add(swordOfMight);
+
+        assertEquals(test, testChar.getInvetory());
+
+
+
+    }
+
+    @Test
+    public void addRemoveGear() throws Exception {
+        Character testChar = new Character();
+
+        Gear helmOfSorrow =  new Gear();
+        helmOfSorrow.setName("helm of Sorrow");
+        testChar.addGear(helmOfSorrow);
+
+
+        assertSame(helmOfSorrow, testChar.getInvetory().get(0));
+
+
+        testChar.removeGear(helmOfSorrow);
+
+        assertNull(testChar.getInvetory());
+
+    }
+
+    @Test
+    public void equipItem() throws Exception {
+        Character testChar = new Character();
+
+        Gear helmOfSorrow =  new Gear();
+        helmOfSorrow.setName("helm of Sorrow");
+        testChar.equipItem(helmOfSorrow);
+
+        assertSame(helmOfSorrow, testChar.getActiveGear().get(0));
+
+        Gear helmOfAnger =  new Gear();
+        helmOfSorrow.setName("helm of Anger");
+        testChar.equipItem(helmOfAnger);
+
+        assertSame(helmOfAnger, testChar.getActiveGear().get(0));
+        assertSame(helmOfSorrow, testChar.getInactiveGear().get(0));
+
+    }
+
+    @Test
+    public void retrieveGearInCategory() throws Exception {
+        Character testChar = new Character();
+
+        Gear helmOfSorrow =  new Gear();
+        helmOfSorrow.setName("helm of Sorrow");
+
+        Gear helmOfAnger =  new Gear();
+        helmOfSorrow.setName("helm of Anger");
+
+        Gear swordOfMight =  new Gear();
+        helmOfSorrow.setName("Sword of Might");
+
+
+        testChar.addGear(helmOfSorrow);
+        testChar.addGear(helmOfAnger);
+        testChar.addGear(swordOfMight);
+
+        ArrayList<Gear> listOfHelms = new ArrayList<Gear>();
+        listOfHelms.add(helmOfSorrow);
+        listOfHelms.add(helmOfAnger);
+
+        assertEquals(listOfHelms, testChar.retrieveGearInCategory("Helm"));
+
+
+
+    }
+
+}
