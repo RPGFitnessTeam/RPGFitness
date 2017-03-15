7ed6f5ee7d1cf923d50fe30a7af9023fdadc04b8
wolfystache <ajclark2@wisc.edu>
2017-03-14 19:28:23 -0500

    Arthur added Tips and initial view for goals.
    I created an extendable view list for displaying tips, and created a
    view to display the progress bar that represents a user's progress
    towards a goal. I'm unable to show more than one at the moment. I
    created 16 local tips to test the extendable view list. All of the
    children contain links that will take the user to an article.

---
+++ RPG_Fitness_App/app/src/main/java/co/rpg_fitness_app/android/rpg_fitness_app/fitness_Package/TipMaster.java
@@ -0,0 +1,124 @@
+package co.rpg_fitness_app.android.rpg_fitness_app.fitness_Package;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ExpandableListView;
+
+//import co.rpg_fitness_app.android.rpg_fitness_app.
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import co.rpg_fitness_app.android.rpg_fitness_app.R;
+
+
+public class TipMaster extends AppCompatActivity {
+    private List<Tips> tipList = new ArrayList<Tips>();
+    private List<Tips> exerciseTipList = new ArrayList<Tips>();
+    private List<Tips> nutritionTipList = new ArrayList<Tips>();
+    private List<Tips> weightTipList = new ArrayList<Tips>();
+    private List<Tips> sleepTipList = new ArrayList<Tips>();
+
+        ExpandableListView expandableListView;
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.tip_main);
+            // Sets toolbar text to Tips and Tricks
+            Toolbar mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+            mToolbar.setTitle("Tips and Tricks");
+            setSupportActionBar(mToolbar);
+            fillExerciseTipList();
+            fillNutritionTipList();
+            fillSleepTipList();
+            fillWeightTipList();
+
+            expandableListView = (ExpandableListView) findViewById(R.id.exp_listview);
+            List<String> fitCategories = new ArrayList<String>();
+            HashMap<String, List<Tips>> childList = new HashMap<String, List<Tips>>();
+            String parentTips[] = getResources().getStringArray(R.array.Fitness_Categories);
+            for (String category : parentTips) {
+                fitCategories.add(category);
+            }
+            childList.put(fitCategories.get(0), nutritionTipList);
+            childList.put(fitCategories.get(1), sleepTipList);
+            childList.put(fitCategories.get(2), exerciseTipList);
+            childList.put(fitCategories.get(3), weightTipList);
+            tipAdapter adapt = new tipAdapter(this, fitCategories, childList);
+            expandableListView.setAdapter(adapt);
+
+        }
+        public boolean addTip(int message, String ID, String type, boolean thumbsUp, boolean thumbsDown) {
+            switch (type) {
+                case "exercise":
+                    Tips eType = new Tips(message, ID, type, false, thumbsUp, thumbsDown);
+                    tipList.add(eType);
+                    exerciseTipList.add(eType);
+                    return true;
+                case "nutrition":
+                    Tips nType = new Tips(message, ID, type, false, thumbsUp, thumbsDown);
+                    tipList.add(nType);
+                    nutritionTipList.add(nType);
+                    return true;
+                case "weight":
+                    Tips wType = new Tips(message, ID, type, false, thumbsUp, thumbsDown);
+                    tipList.add(wType);
+                    weightTipList.add(wType);
+                    return true;
+                case "sleep":
+                    Tips sType = new Tips(message, ID, type, false, thumbsUp, thumbsDown);
+                    tipList.add(sType);
+                    sleepTipList.add(sType);
+                    return true;
+                }
+                return false;
+            }
+            public boolean getNextExcerciseTip() {
+                return true;
+            }
+            public boolean getNextNutritionTip() {
+                return true;
+            }
+            public boolean getNextWeightTip() {
+                return true;
+            }
+            public boolean getNextSleepTip() {
+                return true;
+            }
+            public boolean getNextRandomTip() {
+                return true;
+            }
+            public void fillNutritionTipList() {
+                this.addTip(R.string.n0, UUID.randomUUID().toString(), "nutrition", false, false);
+                this.addTip(R.string.n1, UUID.randomUUID().toString(), "nutrition", false, false);
+                this.addTip(R.string.n2, UUID.randomUUID().toString(), "nutrition", false, false);
+                this.addTip(R.string.n3, UUID.randomUUID().toString(), "nutrition", false, false);
+            }
+            public void fillExerciseTipList() {
+                this.addTip(R.string.e0, UUID.randomUUID().toString(), "exercise", false, false);
+                this.addTip(R.string.e1, UUID.randomUUID().toString(), "exercise", false, false);
+                this.addTip(R.string.e2, UUID.randomUUID().toString(), "exercise", false, false);
+                this.addTip(R.string.e3, UUID.randomUUID().toString(), "exercise", false, false);
+            }
+            public void fillSleepTipList() {
+                this.addTip(R.string.s0, UUID.randomUUID().toString(), "sleep", false, false);
+                this.addTip(R.string.s1, UUID.randomUUID().toString(), "sleep", false, false);
+                this.addTip(R.string.s2, UUID.randomUUID().toString(), "sleep", false, false);
+                this.addTip(R.string.s3, UUID.randomUUID().toString(), "sleep", false, false);
+
+            }
+            public void fillWeightTipList() {
+                this.addTip(R.string.w0, UUID.randomUUID().toString(), "weight", false, false);
+                this.addTip(R.string.w1, UUID.randomUUID().toString(), "weight", false, false);
+                this.addTip(R.string.w2, UUID.randomUUID().toString(), "weight", false, false);
+                this.addTip(R.string.w3, UUID.randomUUID().toString(), "weight", false, false);
+             }
+    }
+
+
+
+
+
---
+++ RPG_Fitness_App/app/src/main/java/co/rpg_fitness_app/android/rpg_fitness_app/fitness_Package/tipAdapter.java
@@ -0,0 +1,108 @@
+package co.rpg_fitness_app.android.rpg_fitness_app.fitness_Package;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+
+import java.util.HashMap;
+import java.util.List;
+
+import co.rpg_fitness_app.android.rpg_fitness_app.R;
+
+/**
+ * Created by Arthur Clark on 3/12/2017.
+ */
+
+public class tipAdapter extends BaseExpandableListAdapter {
+    private List<String> fitnessCategories;
+    private HashMap<String,List<Tips>> tipMap;
+    private Context ctx;
+    public tipAdapter(Context ctx, List<String> fitnessCategories, HashMap<String,List<Tips>> tipMap)
+    {
+        this.ctx = ctx;
+        this.fitnessCategories = fitnessCategories;
+        this.tipMap = tipMap;
+    }
+    @Override
+    public int getGroupCount() {
+        return fitnessCategories.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+
+        return tipMap.get(fitnessCategories.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return fitnessCategories.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return tipMap.get(fitnessCategories.get(groupPosition)).get(childPosition).getMessage();
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean b, View view, ViewGroup viewGroup) {
+        String title = (String)this.getGroup(groupPosition);
+        if (view == null) {
+            LayoutInflater layoutInflate = (LayoutInflater)this.ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflate.inflate(R.layout.parent_layout, null);
+        }
+
+        TextView textView = (TextView)view.findViewById(R.id.heading_item);
+        textView.setTypeface(null, Typeface.BOLD);
+        textView.setText(title);
+        return view;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean b, View view, ViewGroup viewGroup) {
+        int title = (int)this.getChild(groupPosition, childPosition);
+        if (view == null) {
+            LayoutInflater layoutInflate = (LayoutInflater) this.ctx.getSystemService
+                    (Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflate.inflate(R.layout.child_layout, null);
+        }
+        if (childPosition == tipMap.get(fitnessCategories.get(groupPosition)).size() - 1) {
+            view.setPadding(0, 0, 0, 0);
+        } else
+            view.setPadding(0, 0, 0, 20);
+
+        TextView textView = (TextView)view.findViewById(R.id.child_item);
+        textView.setText(title);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+        return view;
+
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
