03a3fa729089f4089cc3452ee8648b198ef75c4b
CptMorgan <amoore8765@gmail.com>
2017-04-05 20:55:39 -0500

    New LogEntry Constructor
    Added a constructor in the LogEntry class to resolve DataSource
    conflicts.

--- RPG_Fitness_App/app/src/main/java/co/rpg_fitness_app/android/rpg_fitness_app/fitness_Package/LogEntry.java
+++ RPG_Fitness_App/app/src/main/java/co/rpg_fitness_app/android/rpg_fitness_app/fitness_Package/LogEntry.java
@@ -3,6 +3,7 @@ package co.rpg_fitness_app.android.rpg_fitness_app.fitness_Package;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public class LogEntry {
 
@@ -34,6 +35,11 @@ public class LogEntry {
     private int intensity = -1; // also, quality of sleep
     private int weight = -1;
     private String typeName = null;
+
+    public LogEntry(int activity) {
+        this.ID = UUID.randomUUID().toString();
+        this.activity = activity;
+    }
 
 
     public LogEntry(String ID, int activity)
